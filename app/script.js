52ec4123cabc0b5cf1f28ea9a3a7f194c5186c72
Alexey <amayzet@mail.ru>
2024-07-05 10:30:37 +0300

    new

--- app/script.js
+++ app/script.js
@@ -1,14 +1,32 @@
 
 
 $(window).ready(function(){
-    $(".boton").wrapInner('<div class=botontext></div>');
+    $(".boton1").wrapInner('<div class=botontext1></div>');
         
-        $(".botontext").clone().appendTo( $(".boton") );
+        $(".botontext1").clone().appendTo( $(".boton1") );
         
-        $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');
+        $(".boton1").append('<span class="twist1"></span><span class="twist1"></span><span class="twist1"></span><span class="twist1"></span>');
         
-        $(".twist").css("width", "25%").css("width", "+=3px");
+        $(".twist1").css("width", "25%").css("width", "+=3px");
     });
+$(window).ready(function(){
+        $(".boton2").wrapInner('<div class=botontext2></div>');
+            
+            $(".botontext2").clone().appendTo( $(".boton2") );
+            
+            $(".boton2").append('<span class="twist2"></span><span class="twist2"></span><span class="twist2"></span><span class="twist2"></span>');
+            
+            $(".twist2").css("width", "25%").css("width", "+=3px");
+        });
+ $(window).ready(function(){
+            $(".boton3").wrapInner('<div class=botontext3></div>');
+                
+                $(".botontext3").clone().appendTo( $(".boton3") );
+                
+                $(".boton3").append('<span class="twist3"></span><span class="twist3"></span><span class="twist3"></span><span class="twist3"></span>');
+                
+                $(".twist3").css("width", "25%").css("width", "+=3px");
+            });
 
 const images = document.querySelectorAll('.slider .slider-line img');
 const sliderLine = document.querySelector('.slider .slider-line');
