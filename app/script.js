71e51f6f375f8aa57ce26ce8d84aa80feb0cb114
Alexey <amayzet@mail.ru>
2024-07-08 10:08:08 +0300

    new slider

--- app/script.js
+++ app/script.js
@@ -32,41 +32,11 @@ $(window).ready(function () {
     $('.twist3').css('width', '25%').css('width', '+=3px')
 })
 
-const images = document.querySelectorAll('.slider .slider-line img')
-const sliderLine = document.querySelector('.slider .slider-line')
-let count = 0
-let width
-
-function init() {
-    console.log('resize')
-    width = document.querySelector('.slider').offsetWidth
-    sliderLine.style.width = width * images.length + 'px'
-    images.forEach((item) => {
-        item.style.width = width + 'px'
-        item.style.height = 'auto'
-    })
-    rollSlider()
-}
-
-init()
-window.addEventListener('resize', init)
-
-document.querySelector('.slider-next').addEventListener('click', function () {
-    count++
-    if (count >= images.length) {
-        count = 0
+let counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 7) {
+        counter = 1;
     }
-    rollSlider()
-})
-
-document.querySelector('.slider-prev').addEventListener('click', function () {
-    count--
-    if (count < 0) {
-        count = images.length - 1
-    }
-    rollSlider()
-})
-
-function rollSlider() {
-    sliderLine.style.transform = 'translate(-' + count * width + 'px)'
-}
+}, 4000);
