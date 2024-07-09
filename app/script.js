4229f0df57fa6b2d31e80cddbdceb214ffb3a6d9
Vlad0s <elky.palki1000@gmail.com>
2024-07-09 14:24:35 +0300

    Приводим код в порядок

--- app/script.js
+++ app/script.js
@@ -20,6 +20,7 @@ if (animatedItems.length > 0) {
             if (itemHeight > window.innerHeight) {
                 itemAnimPoint = window.innerHeight - window.innerHeight / animStart
             }
+            // Да, 400 - это костыль, увы
             if((scrollY > itemOffset - itemAnimPoint - 400) && scrollY < (itemOffset + itemHeight)) {
                 item.classList.add('_active')
             }
@@ -38,46 +39,17 @@ if (animatedItems.length > 0) {
     }, 300)
 }
 
-$(window).ready(function () {
-    $('.boton1').wrapInner('<div class=botontext1></div>')
-
-    $('.botontext1').clone().appendTo($('.boton1'))
-
-    $('.boton1').append(
-        '<span class="twist1"></span><span class="twist1"></span><span class="twist1"></span><span class="twist1"></span>'
-    )
-
-    $('.twist1').css('width', '25%').css('width', '+=3px')
-})
-$(window).ready(function () {
-    $('.boton2').wrapInner('<div class=botontext2></div>')
-
-    $('.botontext2').clone().appendTo($('.boton2'))
-
-    $('.boton2').append(
-        '<span class="twist2"></span><span class="twist2"></span><span class="twist2"></span><span class="twist2"></span>'
-    )
-
-    $('.twist2').css('width', '25%').css('width', '+=3px')
-})
-$(window).ready(function () {
-    $('.boton3').wrapInner('<div class=botontext3></div>')
-
-    $('.botontext3').clone().appendTo($('.boton3'))
-
-    $('.boton3').append(
-        '<span class="twist3"></span><span class="twist3"></span><span class="twist3"></span><span class="twist3"></span>'
-    )
-
-    $('.twist3').css('width', '25%').css('width', '+=3px')
-})
-
+/*
+ *
+ * АВТОМАТИЧЕСКОЕ ПРОЛИСТЫВАНИЕ СЛАЙДЕРА
+ *
+*/
 let counter = 1;
-setInterval(function() {
+setInterval(() => {
     document.getElementById('radio' + counter).checked = true;
     counter++;
     if (counter > 7) {
         counter = 1;
     }
-}, 4000);
+}, 6000);
 
